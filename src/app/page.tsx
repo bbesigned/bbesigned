24f72a8873b004bb21adb5ac8ea5ae841993f356
MrApostl <re4ickaja@gmail.com>
2025-04-13 19:52:03 +0300

    feat: add Header component with logo, button, and menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
+import Header from "../components/Header/Header";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
+				<Header/>
 				<Image
 					className={styles.logo}
 					src="/next.svg"
