b5a385216262180064bba3a38c81ddd5022354ea
wildnhl <minnesotanhlwild@gmail.com>
2025-05-02 18:55:10 +0300

    fix: BEM names

--- src/app/projectExamples/page.tsx
+++ src/app/projectExamples/page.tsx
@@ -7,35 +7,34 @@ import { UpButton } from "../../components/upButton/upButton";
 import style from "./projectExamples.module.scss";
 
 export default function ProjectExamples() {
-	const projectExampleProcessBlockName = "projectExample__process__block-name";
 	return (
 		<>
-			<div className={style.projectExample}>
-				<div className={style.projectExample__info}>
-					<div className={style.projectExample__infoHead}>
-						<p className={style.projectExample__infoYear}>2024</p>
-						<h2 className={style.projectExample__headName}>Project name</h2>
+			<div className={style.projectExampleWrap}>
+				<div className={style.projectExampleContainer}>
+					<div className={style.infoHeadWrap}>
+						<p className={style.infoHeadWrap__title}>2024</p>
+						<h2 className={style.infoHeadWrap__subtitle}>Project name</h2>
 					</div>
-					<div className={style.projectExample__process}>
-						<div className={style.projectExample__processWrapper}>
-							<p className={style[projectExampleProcessBlockName]}>Process:</p>
-							<div className={style.projectExample__processList}>
+					<div className={style.processWrapper}>
+						<div className={style.processContainer}>
+							<p className={style.processWrapper__headTitle}>Process:</p>
+							<div className={style.processContainerTitleList}>
 								<p>UX/UI</p>
 								<p>IA</p>
 								<p>Design System</p>
 								<p>Mobile</p>
 							</div>
 						</div>
-						<p className={style.projectExample__process__programmingText}>Programming</p>
+						<p className={style.processWrapper__title}>Programming</p>
 					</div>
-					<div className={style.projectExample__client}>
-						<p className={style[projectExampleProcessBlockName]}>Client:</p>
-						<p className={style.projectExample__client__companyName}>Company name</p>
+					<div className={style.clientWrap}>
+						<p className={style.processWrapper__headTitle}>Client:</p>
+						<p className={style.clientWrap__title}>Company name</p>
 					</div>
-					<div className={style.projectExample__summary}>
-						<p className={style[projectExampleProcessBlockName]}>Summary:</p>
-						<div>
-							<p className={style.projectExample__summary__text}>
+					<div className={style.summaryWrapper}>
+						<p className={style.processWrapper__headTitle}>Summary:</p>
+						<div className={style.summaryContainer}>
+							<p className={style.summaryContainer__titleContent}>
 								Beyond technical execution, the project aims to act as a strategic partner, helping
 								brands expand their user base, boost revenue, and thrive in the digital realm. With
 								a commitment to client satisfaction, the team is dedicated to delivering innovative
