85999644c0ba007a06fe3fa71fd45ce2d4952b8a
MrApostl <63881079+MrApostl@users.noreply.github.com>
2025-05-06 23:44:23 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Banner from "../components/banner/banner";
 
+import Link from "next/link";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
 	return (
 		<div className={`${styles.page}`}>
 			<Banner />
+			<Link href={"/projectExamples"}>Project Examples</Link>
 		</div>
 	);
 }
