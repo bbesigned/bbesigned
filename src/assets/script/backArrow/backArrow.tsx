76274b6e6eece1b944eee3933c39dd6858e3f904
wildnhl <minnesotanhlwild@gmail.com>
2025-05-09 17:36:40 +0300

    fix: props name fix

--- src/assets/script/backArrow/backArrow.tsx
+++ src/assets/script/backArrow/backArrow.tsx
@@ -1,6 +1,7 @@
 import { type IIconsProps } from "types/common/PageProps";
+import { colorPalette } from "helpers/colorPalette";
 
-export const BackArrow = ({ colorArrow = "#FFF8F0", classNames }: IIconsProps) => (
+export const BackArrow = ({ color = colorPalette.floralWhite, classNames }: IIconsProps) => (
 	<svg
 		className={classNames}
 		width="33"
@@ -10,14 +11,14 @@ export const BackArrow = ({ colorArrow = "#FFF8F0", classNames }: IIconsProps) =
 		xmlns="http://www.w3.org/2000/svg">
 		<path
 			d="M25.8561 17H6.87305"
-			stroke={colorArrow}
+			stroke={color}
 			strokeWidth="2.0339"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		/>
 		<path
 			d="M16.3646 26.4909L6.87305 16.9993L16.3646 7.50781"
-			stroke={colorArrow}
+			stroke={color}
 			strokeWidth="2.0339"
 			strokeLinecap="round"
 			strokeLinejoin="round"
