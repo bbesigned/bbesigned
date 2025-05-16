046e0f386b5a95573ed995a9b4fbed4e794be1a5
wildnhl <minnesotanhlwild@gmail.com>
2025-05-16 20:33:13 +0300

    fix: review fix

--- src/components/controlButtons/controlButtons.tsx
+++ src/components/controlButtons/controlButtons.tsx
@@ -9,14 +9,14 @@ import style from "./controlButtons.module.scss";
 export const ControlButtons = ({ classnames }: IControlButtons) => {
 	return (
 		<div className={cn(style.controlButtons, classnames)}>
-			<button className={`${style.controlButtons__btn} ${style.controlButtons__btn_left}`}>
+			<button className={cn(style.controlButtons__btn, style.controlButtons__btn_left)}>
 				<ControlArrow classNames={style.controlButtons__arrow} />
 				<span>Previous project</span>
 			</button>
-			<button className={`${style.controlButtons__btn} ${style.controlButtons__btn_viewAll}`}>
+			<button className={cn(style.controlButtons__btn, style.controlButtons__btn_viewAll)}>
 				View All
 			</button>
-			<button className={`${style.controlButtons__btn} ${style.controlButtons__btn_right}`}>
+			<button className={cn(style.controlButtons__btn, style.controlButtons__btn_right)}>
 				<span>Next Project</span>
 				<ControlArrow classNames={style.controlButtons__arrow} right />
 			</button>
