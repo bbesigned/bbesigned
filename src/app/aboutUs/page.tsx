451d5630fda4694608ddbeab6be56d72e676edfe
Linchit21 <wladev.it@gmail.com>
2025-07-06 15:46:07 +0200

    feat: continue work

---
+++ src/app/aboutUs/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from "next/image";
+
+import BannerHeader from "components/bannerHeader/bannerHeader";
+
+import BannerFooter from "components/bannerFooter/bannerFooter";
+
+import aboutUs from "../../../public/aboutUs.png";
+
+import styles from "./aboutUs.module.scss";
+
+const AboutUs = () => {
+	return (
+		<div className={styles.aboutUs}>
+			<div className={styles.aboutUs__rectangle}></div>
+			<div className={styles.aboutUs__header}>
+				<BannerHeader isDark={true} smallLogo={true}></BannerHeader>
+			</div>
+			<div className={styles.aboutUs__content}>
+				<div className={styles.aboutUs__scrollContainer}>
+					<div className={styles.aboutUs__scrollbar}></div>
+					<div className={styles.aboutUs__block}>
+						<div className={styles.aboutUs__title}>about us</div>
+						<div className={styles.aboutUs__info}>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eligendi ullam
+							consectetur maiores delectus dolores molestiae, velit, nesciunt quasi ea doloremque ut
+							nostrum amet soluta praesentium. Alias, minus! Ducimus, tempore! Voluptatum minus
+							eveniet inventore repellendus in iusto, accusamus dicta quidem soluta nam perspiciatis
+							quisquam ex tempore et aliquid, sint eligendi? Totam fugit ad illo aliquam, autem
+							omnis vero odit dolor. Voluptatibus, ab dolore. At praesentium provident adipisci
+							itaque, beatae modi temporibus. Nihil eius itaque ipsam maxime perspiciatis, officiis
+							minima dolore ea recusandae facere saepe enim culpa excepturi sapiente placeat
+							explicabo? Repellat repudiandae corporis nesciunt, eius dolorem placeat quisquam iusto
+							assumenda sint saepe laboriosam architecto deserunt doloremque quis id rem eaque ea
+							quo dolorum delectus sunt! Vitae et quaerat cum voluptate? Quae odit alias repellendus
+							iusto ab illum error ea adipisci, laudantium maiores velit, magni quam vero. Facilis
+							odit, animi omnis dicta similique doloremque est, expedita sint fugit neque
+							repellendus quos. Animi, corrupti accusamus eligendi nobis numquam a odio quidem iure
+							consequatur, nulla perferendis? Ea necessitatibus quas eveniet impedit aliquid nam
+							esse optio quis. Iure pariatur dolorum, cupiditate ipsum saepe aut? Nihil neque
+							aliquam itaque? Reprehenderit recusandae sapiente consequatur eveniet, dolorem animi
+							voluptatem cum ad numquam? Non distinctio asperiores est rem, doloribus eos minus quos
+							obcaecati debitis, blanditiis nisi, itaque expedita. Libero eius dignissimos ullam non
+							corrupti neque inventore nesciunt molestiae officiis, cupiditate esse, harum expedita
+							ipsam accusantium sapiente, doloremque unde ea eos accusamus porro? Repudiandae quos
+							inventore at eligendi aspernatur. Quo maxime maiores, quasi similique est cum quisquam
+							fugit non quod repudiandae labore illum ea, nisi voluptatum delectus assumenda
+							praesentium harum minima. Repudiandae dolor repellendus vero amet, dolore voluptatum
+							natus! Harum exercitationem aliquam, nulla dolores assumenda quia ad a animi
+							voluptatibus ratione reiciendis odio eos incidunt illo excepturi dolore debitis!
+							Magnam numquam repellat id iste blanditiis similique commodi itaque ipsam. Hic,
+							laborum excepturi quidem, voluptas temporibus fugiat reiciendis illum ab rerum unde,
+							accusantium dicta exercitationem sequi recusandae corrupti nihil officia? Illo rem
+							aperiam fugit, harum consectetur rerum dolor libero asperiores. Laborum dolor odio
+							laudantium quas beatae doloremque culpa, eaque praesentium architecto inventore, iusto
+							consectetur reprehenderit rem eligendi sunt, autem debitis maxime cupiditate
+							voluptatum reiciendis harum explicabo omnis repudiandae nemo! Doloribus. Facilis,
+							laborum magni? Quam perferendis repellendus excepturi eligendi reiciendis dolorum,
+							quibusdam molestiae libero architecto labore repudiandae, dolor consequatur aliquid
+							voluptate eos maxime illo rerum fugiat, alias commodi. Iusto, tempore id! Voluptatem
+							rerum qui voluptatum! Sit cupiditate laborum sapiente deserunt, autem vel quo et modi,
+							animi, officiis eum! Est, fugiat! Ea nisi eligendi adipisci nam aut impedit modi at
+							omnis iure. Architecto assumenda aspernatur cumque at magni ipsam similique eligendi
+							tenetur quod obcaecati tempora repellendus, ad optio libero dignissimos, asperiores
+							odio ipsa distinctio sit. Enim consequatur assumenda quos iste dolor fugit? Quo,
+							recusandae? Nobis sequi, vel repudiandae sit sapiente recusandae alias cumque, omnis
+							excepturi molestiae dolores, atque quaerat esse architecto? At cumque odio vel
+							consectetur maxime? Quas laboriosam quibusdam ipsum maxime. Libero, saepe et?
+							Doloribus doloremque, commodi mollitia atque ea, fugit assumenda numquam amet optio
+							cumque laborum earum officiis natus cum! Laudantium quam distinctio dolore natus
+							quibusdam corrupti reprehenderit quaerat alias! Ab totam nostrum sint assumenda
+							quibusdam quaerat, dicta perspiciatis dolores similique nulla nihil dolorem,
+							consectetur dolor esse reprehenderit adipisci fugiat corporis maiores. Doloribus
+							accusantium error ea ipsum cupiditate rerum iusto. Quae perferendis omnis tenetur
+							laboriosam animi consequatur dolor ipsam? Amet obcaecati aperiam beatae. Dolorem at
+							magni labore! Est voluptas natus ab necessitatibus repellat, esse veritatis molestiae
+							ullam dicta temporibus suscipit. Quae aliquam, culpa laboriosam architecto excepturi
+							distinctio magnam mollitia vitae ipsum ratione sapiente molestias dicta! Quis,
+							molestiae eligendi. Totam vero quo facilis impedit delectus deleniti praesentium quia.
+							Dolorem, dicta et!
+						</div>
+					</div>
+				</div>
+
+				<Image
+					// className={styles.badConnectContainer__image}
+					src={aboutUs}
+					width={768}
+					height={1127}
+					alt="besigned"
+				/>
+			</div>
+			<BannerFooter />
+		</div>
+	);
+};
+
+export default AboutUs;
